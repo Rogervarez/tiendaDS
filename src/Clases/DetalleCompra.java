9387ac3345a5e4d907b7d82ba653a985f602a028
HazaelLopezDW <elhazael134@gmail.com>
2017-04-03 09:19:06 -0600

    Cambio DetalleCompra

--- src/Clases/DetalleCompra.java
+++ src/Clases/DetalleCompra.java
@@ -10,11 +10,55 @@ package Clases;
  * @author HAZAEL
  */
 public class DetalleCompra {
+    
     private Producto producto;
     private int cantidad;
-    private double costoUnitario;
+    protected double costoUnitario;
     
+    // Primer Constructor Instanciable si parametro 
     public DetalleCompra(){
+        // llamada implicita
+    }
+    
+    // Segundo Constructor Instanciable Con Parametros
+    public DetalleCompra( Producto producto, int cantidad, double costoUnitario ){
         
+        setCantidad(cantidad);
+        setCostoUnitario(costoUnitario);
+        
+        this.producto = producto;
+        this.cantidad = getCantidad();
+        this.costoUnitario = getCostoUnitario();
+    }
+    
+    
+    // Declaracion de los metos establecer y obtener
+    public void setCantidad( int valCantidad ){
+        if( valCantidad > 0 ){
+            cantidad = valCantidad;
+        }
+        else {
+            cantidad = 0;
+        }
+    }
+    
+    public int getCantidad(){
+        return cantidad;
+    }
+    
+    public void setCostoUnitario( double valCostoUnitario ){
+        if( valCostoUnitario > 0.0 ){
+            costoUnitario = valCostoUnitario;
+        }
+        else {
+            costoUnitario = 0.0;
+        }
+    }
+    
+    public double getCostoUnitario(){
+        return costoUnitario;
+    }
+    public String toString(){
+        return "El producto es " + producto.toString() + "\t" + "Su Costo por unidad es" + costoUnitario;
     }
 }
