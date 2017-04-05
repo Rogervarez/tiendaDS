7aac99ed26227b48687e1cf5033dd3e3490028c6
Ragon <rogelioalvarez2@hotmail.com>
2017-04-04 20:52:50 -0700

    Clase Detalle Compra terminada

--- src/Clases/DetalleCompra.java
+++ src/Clases/DetalleCompra.java
@@ -7,14 +7,58 @@ package Clases;
 
 /**
  *
- * @author HAZAEL
+ * @author Roger
  */
 public class DetalleCompra {
     private Producto producto;
     private int cantidad;
     private double costoUnitario;
     
-    public DetalleCompra(){
-        
+    public DetalleCompra(Producto producto, int cantidad, double costoUnitario){
+        this.producto = producto;
+        this.cantidad = cantidad;
+        this.costoUnitario = costoUnitario;
+    }
+
+    /**
+     * @return the cantidad
+     */
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    /**
+     * @param cantidad the cantidad to set
+     */
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    /**
+     * @return the costoUnitario
+     */
+    public double getCostoUnitario() {
+        return costoUnitario;
+    }
+
+    /**
+     * @param costoUnitario the costoUnitario to set
+     */
+    public void setCostoUnitario(double costoUnitario) {
+        this.costoUnitario = costoUnitario;
+    }
+
+    /**
+     * @return the producto
+     */
+    public Producto getProducto() {
+        return producto;
+    }
+
+    /**
+     * @param producto the producto to set
+     */
+    public void setProducto(Producto producto) {
+        this.producto = producto;
     }
 }
