32ee98935ab7ad643419dbb988e112a43ae387d0
Ragon <rogelioalvarez2@hotmail.com>
2017-04-04 19:11:45 -0700

    Inicio clase parametro

--- src/Clases/Parametro.java
+++ src/Clases/Parametro.java
@@ -5,21 +5,43 @@
  */
 package Clases;
 
+import connections.conection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 /**
  *
  * @author HAZAEL
  */
 public class Parametro {
-    private String idParametro;
-    private String nombre;
-    private String valor;
+    public String idParametro;
+    public String nombre;
+    public String valor;
     
     public Parametro(){
         
     }
     
-    public void Obtener( Parametro[] parametro ){
-        
+    public void Obtener(){
+        String[] cm = new String[]{"idParametro", "Nombre", "Valor"};
+        conection cn = new conection();
+        try {
+            cn.Conectar();
+            PreparedStatement ps = cn.BuscarTodos("parametro", cm);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                T.model.addElement(rs.getString("idespecialidadMD") + " --- " + rs.getString("nombreEspecialidadMD"));
+            }
+            T.lst_esp1.setModel(T.model);
+            cn.Desconectar();
+        } catch (Exception ex) {
+            try {
+                print.mensaje("Algo ha impedido la lectura de datos: " + ex.getMessage() + "\n" + "Aparentemente el usuario buscado no exixte." + "\n" + "Busque nuevamente con otro ID", "Error ML.guardarConsulta");
+                cn.Desconectar();
+            } catch (Exception o) {
+                print.mensaje("Un grabe error se ha dado mientra usted intentaba buscar paciente: El servidor podría estar apagado, enciéndalo. Intentelo de nuevo y si el error persiste llame a su proveedor" + o.getMessage(), "Error ML.buscarPaciente");
+            }
+        }
     }
     
     public void ObtenerUtilidad( Parametro parametro ){
